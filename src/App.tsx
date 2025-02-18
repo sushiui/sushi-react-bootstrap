df1016949f33a397e275eecab34e8a1d8419452f
Apirak Panatkool <apirakb@gmail.com>
2025-02-18 17:42:52 +0700

    Test SsSite SsHeader SsBody

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,23 @@ import './App.css'
 import { Button, HelloWorld } from './index'
 import './styles/index.scss';
 import SsButton from './components/SsButton/SsButton'
+import SsPage from './components/SsPage/SsPage';
+import SsHeader from './components/SsHeader/SsHeader';
+import SsBody from './components/SsBody';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Sushi Bootstrap Test</h1>
-      
-      {/* Test the exported React Bootstrap Button */}
-      <Button variant="primary">Test Button</Button>
-
-      <SsButton variant="primary">Test SsButton</SsButton>
-      
-      {/* Test your custom HelloWorld component */}
-      <HelloWorld />
-    </div>
+    <SsPage>
+      <SsHeader className='-nobreadcrumbs'>
+        <span className='lable'>Sushi Bootstrap Test</span>
+      </SsHeader>
+      <SsBody>
+        <h1>Sushi Bootstrap Test</h1>
+        <SsButton variant="primary">Test Button</SsButton>
+      </SsBody>
+    </SsPage>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-createRoot(document.getElementById('root')!).render(
+const container = document.getElementById('root') 
+const root = createRoot(container!)
+
+root.render(
   <StrictMode>
     <App />
   </StrictMode>,
