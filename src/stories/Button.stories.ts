71df2c7926f4ecaa86b18532c214a892d334e582
Apirak Panatkool <apirakb@gmail.com>
2025-08-06 11:09:57 +0700

    Add type annotations for meta in Button and Header story files

--- src/stories/Button.stories.ts
+++ src/stories/Button.stories.ts
@@ -4,7 +4,7 @@ import { fn } from '@storybook/test';
 import { Button } from './Button';
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
-const meta = {
+const meta: Meta<typeof Button> = {
   title: 'Example/Button',
   component: Button,
   parameters: {
